40bd2b30e7fb3ebec66787df509612ce0af828be
eyp <eypsay@yahoo.com>
2026-09-10 15:54:00 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,111 @@
+use topcoat::{Result,
+              router::{Router,RouterBuilderDiscoverExt,page},
+view::{component,view}
+};
+use topcoat::view::internal::view;
+
+#[tokio::main]
+async fn  main() {
+    topcoat::start(Router::builder().discover().build()).await.unwrap();
+
+    //println!("Hello, world!");
+}
+
+#[page("/")]
+async fn home() -> Result {
+    view!{
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <title>"SHELF"</title>
+                topcoat::dev::script()
+            </head>
+            // <body>hello(name: "EEYP")</body>
+        <body>
+        book_card(title:"sefiller",
+            author:"Dost",
+            year: 2020,
+            finished:true
+        )
+        book_card(title:"reziller",
+            author:"hugo",
+            year: 2000,
+            finished:true
+        )
+        book_card(title:"filler",
+            author:"elt",
+            year: 2010,
+            finished:false
+        )
+        </body>
+        </html>
+    }
+}
+// #[component]
+// async fn hello(name:&str) -> Result {
+//     let book_count=4;
+//     let genres= vec!["Science","History","Potery"];
+//     let is_logged_in=true;
+//     view!{
+//         <h1>
+//             "Hello,"
+//             (name)
+//             "!"
+//         </h1>
+//         // <p>
+//         //     "you have "
+//         //     (book_count)
+//         //     " books on the shelf"
+//         // </p>
+//         // <a href="/about">"About this site"</a>
+//         // <p title=(name)>"Hover me."</p>
+//
+//         <ul>
+//             for genre in &genres {
+//                 <li>(genre)</li>
+//             }
+//         </ul>
+//
+//         // if is_logged_in {
+//         //     <p>"Signed in"</p>
+//         // }else{
+//         <a
+//             href="/login"
+//             if is_logged_in {
+//                 class="active"
+//                 aria-current="page"
+//             }
+//         >
+//             "Sign In!"
+//         </a>
+//     }
+// }
+
+#[component]
+async fn book_card(title:&str,author:&str,year:u32,finished:bool) -> Result {
+view!{
+    <article>
+        <h2>(title)</h2>
+        <p>
+            (author)
+            " - "
+            (year)
+        </p>
+        <span
+            class="badge"
+            if finished {
+                class="badge done"
+            }
+        >
+            if finished {
+                "FINISHED"
+            } else {
+                "Unread"
+            }
+        </span>
+    </article>
+}
+
+
+}
+
